1860cff42af88bfef3057661f0cc862db4df596a
Seed <hsik0225@gmail.com>
2021-12-25 07:13:12 +0900

    feat: setup project

---
+++ src/main/java/com/hyeonsik/notionadapter/NotionAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonsik.notionadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionAdapterApplication.class, args);
+	}
+
+}
