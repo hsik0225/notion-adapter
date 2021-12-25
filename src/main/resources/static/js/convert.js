e712ca16ca320882d3aa28cd975da0c8f1abca2d
Seed <hsik0225@gmail.com>
2021-12-26 08:39:51 +0900

    IntelliJ에서 Notion으로 글 붙여넣기 호환성 개선 (#8)
    * refactor: js에서 selected를 사용하도록 하여 html에서 option value 제거
    
    * feat: textarea로부터 텍스트를 입력받아오는 기능 구현
    
    * feat: 이미지 링크 앞에 CRLF 추가하기 구현

---
+++ src/main/resources/static/js/convert.js
@@ -0,0 +1,33 @@
+const $CONVERT_BUTTON = $("#convert-button");
+
+const REGEX = /\s*!\[([\w\s\d]+)\]\((.*\.png|jpg)\)/igm;
+
+function toIntelliJ() {
+    $CONVERT_BUTTON.replaceWith(`<button id="cancel-button" onclick="">Cancel</button>`);
+    $CONVERT_BUTTON.replaceWith(`<button id="convert-button" onclick="toIntelliJ()">Convert</button>`);
+}
+
+function toNotion() {
+    $CONVERT_BUTTON.replaceWith(`<button id="cancel-button" onclick="">Cancel</button>`);
+    let content = $("#text").val();
+
+    const lines = content.split('\n');
+    for (let i = 0; i < lines.length; i++) {
+        let depth = "";
+        if (lines[i].startsWith(" ")) {
+            const count = lines[i].search(/\S/g);
+            if (count >= 0) {
+                depth = "".repeat(count);
+            }
+        }
+
+        Array.from(lines[i].matchAll(REGEX))
+            .map(matches => matches[0])
+            .forEach(match => content = content.replace(match, "\r\n\r\n" + depth + match))
+    }
+
+    $("#result").replaceWith(
+              `<textarea id="result" name="text" cols="80" rows="15" wrap="off">${content}</textarea>`);
+
+    $CONVERT_BUTTON.replaceWith(`<button id="convert-button" onclick="toNotion()">Convert</button>`);
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,21 +5,17 @@ $(document).ready(function () {
 function decideToEditor() {
     $("#content").empty();
 
-    const fromEditors = $("#from-editor").val();
-    if ("notion" === fromEditors) {
+    const fromEditors = $("#from-editor :selected").text();
+    if ("Notion" === fromEditors) {
         showNotionToIntelliJForm();
     } else {
         showIntelliJToNotionForm();
     }
-
-    showConvertButton();
 }
 
 function showNotionToIntelliJForm() {
-    const toEditors = "IntelliJ";
-    $("#to-editor").html("To: " + toEditors);
-    $("#content").append(
-        `<div id="setting">
+    showForm("IntelliJ",
+        `<div id="option">
             <label> Notion Link: <input type="text"></label><br>
             <label> Title: <input type="text"></label><br>
             <label> Date: <input type="text"></label><br>
@@ -28,22 +24,14 @@ function showNotionToIntelliJForm() {
 }
 
 function showIntelliJToNotionForm() {
-    const toEditors = "Notion";
+    showForm("Notion",
+        `<textarea id="text" name="text" cols="80" rows="15" wrap="off" onFocus="this.value='';this.onfocus=null;">Enter your text...</textarea>`);
+
+    $("#content").append(`<textarea id="result" name="text" cols="80" rows="15" wrap="off"></textarea>`)
+}
+
+function showForm(toEditors, form) {
     $("#to-editor").html("To: " + toEditors);
-    $("#content").append(
-        `<div id="text">
-            <label>
-                <textarea id="text" name="text" cols="80" rows="15" wrap="off" onFocus="this.value='';this.onfocus=null;">
-                    Enter your text...
-                </textarea>
-            </label>
-        </div>`);
+    $("#content").append(form)
+        .append(`<button id="convert-button" onclick="to${toEditors}()">Convert</button>`);
 }
-
-function showConvertButton() {
-    $("#content").append(
-        `<div id="convert-button">
-            <button class="convert-button" id="to-notion-button">Convert</button>
-        </div>`);
-}
-
